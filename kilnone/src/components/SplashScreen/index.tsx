900b2ff3e0f200e4ecae76ed4cfad1f9d4eafe9c
jeantroiani <jeantroiani@icloud.com>
2020-12-08 00:28:09 +0000

    Bootstrap Create React App

---
+++ kilnone/src/components/SplashScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const SplashScreen = () => {
+    const p = {
+        color: "pink"
+    }
+    return (
+        <>
+            <h1>Kiln.one</h1>
+            <p style={p}>Crafting your ideas into the web</p>
+            <p>We are a multidisciplinary group of designers and programmers specialized in modern website design and brand development for your company, service or product.</p>
+        </>
+    );
+}
