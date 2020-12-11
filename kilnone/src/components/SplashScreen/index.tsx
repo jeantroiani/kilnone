e5476f1bc61add840fdcb4de51a2fbf1859ff36f
Ramiro Gordillo <ramirogordillo@hotmail.com>
2020-12-11 13:15:38 -0500

    Added our deadly logo

--- kilnone/src/components/SplashScreen/index.tsx
+++ kilnone/src/components/SplashScreen/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import logo from './logo.svg';
 
 export const SplashScreen = () => {
     return (
         <React.Fragment>
+            <div className={styles.landing}></div>
+            <img src={logo} alt="Kilnone Logo"/>
             <h1 className={styles.error}>Kiln.one</h1>
             <p>Crafting your ideas into the web</p>
             <p >We are a multidisciplinary group of designers and programmers specialized in modern website design and brand development for your company, service or product</p>
