723e3834ada0692ae50ad2e8a23d88239bf62c0f
jeantroiani <jeantroiani@icloud.com>
2020-12-14 11:08:45 +0000

    Add form at home; update stytle for home;

--- kilnone/src/App.tsx
+++ kilnone/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { SplashScreen, WhatWeOffer  } from './components';
+import { Home, WhatWeOffer  } from './components';
 
 function App() {
   return (
     <div className="App">
-      <SplashScreen />
+      <Home />
       <WhatWeOffer />
     </div>
   );
---
+++ kilnone/src/components/Home/ContactUsForm/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+export const ContactUsForm = () => {
+    return (
+        <>
+            <form className={styles.formContainer} noValidate autoComplete="off">
+                <TextField size="small" id="filled-basic" label="Contact Us" variant="filled" />
+                <Button size="small" className={styles.button}>Submit</Button>
+            </form>
+        </>
+    );
+}
