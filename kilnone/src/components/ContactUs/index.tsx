ed5b86cdfb9cb2db4c39e0693d76d81adad2dce9
Ramiro Gordillo <ramirogordillo@hotmail.com>
2020-12-14 21:38:46 -0500

    Created From card on contact us component, added background and css

--- kilnone/src/components/ContactUs/index.tsx
+++ kilnone/src/components/ContactUs/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { FormControl, InputLabel, Input,Button } from "@material-ui/core";
 import styles from './styles.module.scss';
 import globalStyles from '../Styles/styles.module.scss';
 
 export const ContactUs = () => {
 
     return (
-        <section > 
-            <h1 className={globalStyles.sectionMainTitle}>Contact Us</h1>
-           
+        <section >
+            <div className={styles.contactUsBackground}>
+                <h1 className={styles.sectionMainTitle}>Contact Us</h1>
+                <div className={styles.contactUsForm}>
+                    <form>
+                        <FormControl margin="dense" fullWidth>
+                        <InputLabel htmlFor="name">Name</InputLabel>
+                        <Input id="name" type="text" />
+                        </FormControl>
+
+                        <FormControl margin="dense" fullWidth>
+                        <InputLabel htmlFor="email">Email</InputLabel>
+                        <Input id="email" type="email" />
+                        </FormControl>
+
+                        <FormControl margin="dense" fullWidth>
+                        <InputLabel htmlFor="message">Message</InputLabel>
+                        <Input id="message" multiline rows={6} />
+                        </FormControl>
+
+                        <Button variant="contained" className={globalStyles.button}>Send</Button>
+                    </form>
+                </div>
+            </div>
         </section>
      
     );
