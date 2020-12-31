fbec98565ef035a15d2d300e3a41e64fa8f10a75
Ramiro Gordillo <ramirogordillo@hotmail.com>
2020-12-30 23:33:03 -0500

    Home Section redesigned Cleaned/updated Assets Folder Cleaned global css styles

--- kilnone/src/components/Home/index.tsx
+++ kilnone/src/components/Home/index.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
 import styles from './styles.module.scss';
-// import logo from './logo.svg';
-// import homeBackground from './homeBackground.svg';
-import globalStyles from '../Styles/styles.module.scss';
-import { ContactUsForm } from './ContactUsForm';
+import img from '../Assets/home-Image.png';
+
 export const Home = () => {
     return (
         <>
-        <div className={styles.container}>
-            <div className={styles.informationBackground}>
-                <div className={styles.contentContainer}>
-                    <h1 className={styles.mainTitle}>KILN<span className={globalStyles.sectionMainTitle}>.one</span></h1>
-                    <h2 className={globalStyles.sectionMainTitle}>Crafting your ideas into the web</h2>
-                    <p className={globalStyles.sectionContent}>We are a multidisciplinary group of designers and programmers specialized in modern website design and brand development for your company, service or product</p>
-                    {/* <ContactUsForm /> */}
-                </div>
+            <div className={styles.homeBackground}>
+                <img className={styles.homeImage} src={img} alt="Desktop Computer" />
+                <p className={styles.logoKiln}>KILN.<span className={styles.logoOne}>one</span></p>
+                <p className={styles.kilnMoto}>Crafting your ideas into the web</p>
+                <p className={styles.kilnGreeting}>We are a multidisciplinary group of designers and programmers specialized in modern website design and brand development for your company, service or product</p>
             </div>
-        </div>
         </>
     );
 }
