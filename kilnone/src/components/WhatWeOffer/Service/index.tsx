8576c15d9dec9460cc0699510d4de8c4dd18e508
jeantroiani <jeantroiani@icloud.com>
2021-01-04 22:39:37 +0000

    Services style update

--- kilnone/src/components/WhatWeOffer/Service/index.tsx
+++ kilnone/src/components/WhatWeOffer/Service/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import globalStyles from '../../Styles/styles.module.scss';
+import globalStyles from '../../styles/styles.module.scss';
 import { Button } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Card from '@material-ui/core/Card';
 import styles from './styles.module.scss';
 
-interface ServiceProps {
+export interface ServiceProps {
     action: (event: any)=> void;
     title: string;
     text: string;
+    imageUrl: any;
+    imageAlt: string;
 }
 
 export const Service = (props: ServiceProps) => {
-    const {action, title, text} = props;
+    const {action, title, text, imageUrl, imageAlt} = props;
+    console.log(props)
     return (
-        <Card className={styles.container}> 
-            <CardContent>
-                <h1 className={globalStyles.sectionMainTitle}>{title}</h1>
-                <p>{text}</p>
+        <Card className={styles.container}>
+            <img className={styles.image} src={imageUrl} alt={imageAlt}/>
+            <CardContent className={styles.cardContent}>
+                <h1 className={styles.title}>{title}</h1>
+                <p className={globalStyles.sectionContentMediumDarkNormal}>{text}</p>
             </CardContent>
-            <CardActions>
+            <CardActions className={styles.cardAction}>
             <Button variant="contained" className={globalStyles.button} onClick={action} >More about this</Button>
             </CardActions>
         </Card>
--- kilnone/src/components/WhatWeOffer/index.tsx
+++ kilnone/src/components/WhatWeOffer/index.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import globalStyles from '../Styles/styles.module.scss';
-import { Service } from './Service';
+import globalStyles from '../styles/styles.module.scss';
+import { Service, ServiceProps } from './Service';
+import person from "./app.png";
+import bench from "./bench.png";
 
-interface IService {
-    action: (event: any) => void,
-    title: string,
-    text: string,
-}
+// interface IService {
+//     action: (event: any) => void,
+//     title: string,
+//     text: string,
+// }
 
-const services : Array<IService> = [
+const services : Array<ServiceProps> = [
     {
         action: (event: any) => console.log(event),
         title: "Application development",
-        text: "If functionality is what you are looking for web apps can provide with multi plataform/device solutions."
+        text: "If functionality is what you are looking for web apps can provide with multi plataform/device solutions.",
+        imageUrl: person,
+        imageAlt: "Person looking at yellow board with an iPad"
     },
     {
         action: (event: any) => console.log(event),
         title: "Web development",
-        text: "Websites can provide full inmersive experiences. Interactive and responsive are the name of the game."
+        text: "Websites can provide full inmersive experiences. Interactive and responsive are the name of the game.",
+        imageUrl: bench,
+        imageAlt: "Person looking at yellow board with an iPad"
     },
-        {
+    {
         action: (event: any) => console.log(event),
         title: "Branding",
-        text: "Need a complete solution to your image? Our team can appropriately develop an entire concept to suit your needs."
+        text: "Need a complete solution to your image? Our team can appropriately develop an entire concept to suit your needs.",
+        imageUrl: person,
+        imageAlt: "Person looking at yellow board with an iPad"
     }
 ];
     
@@ -31,17 +39,19 @@ const services : Array<IService> = [
 export const WhatWeOffer = () => {
 
     return (
-        <section > 
-            <h1 className={globalStyles.sectionMainTitle}>What We Offer</h1>
-            <p>Kiln Studio is located in London, UK providing its customers branding, website design, web apps, illustrations and documentation among other services.</p>
-            <p>We strive to produce engaging designs and web experiences by working together with our clients through constant communication and understanding of their needs.</p>
-            <p>We pride ourself in following all our projects through from the moment we begin a relationship with our clients up to the delivery of the final product.</p>
+        <section className={globalStyles.section}> 
+            <h1 className={globalStyles.sectionMainTitleDark}>What we do</h1>
+            <p className={globalStyles.largeDarkSans}>Kiln Studio is located in London, UK providing its customers branding, website design, web apps, illustrations and documentation among other services.
+            We strive to produce engaging designs and web experiences by working together with our clients through constant communication and understanding of their needs.
+            We pride ourself in following all our projects through from the moment we begin a relationship with our clients up to the delivery of the final product.</p>
             <ul className={styles.serviceGroups}>
-             {services.map((service: IService) => <li className={styles.service}>                 
+             {services.map((service: ServiceProps) => <li className={styles.service}>                 
                 <Service 
                         action={service.action}
                         title={service.title}
                         text={service.text}
+                        imageUrl={service.imageUrl}
+                        imageAlt={service.imageAlt}
                     /></li>
              )}
             </ul>
