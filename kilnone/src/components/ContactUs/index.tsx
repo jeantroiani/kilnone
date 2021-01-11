50ac302473099fa0d89b18f15c9f1cc3f7d3442e
Ramiro Gordillo <ramirogordillo@hotmail.com>
2021-01-11 16:10:09 -0500

    Added Contact Us Section (#3)

--- kilnone/src/components/ContactUs/index.tsx
+++ kilnone/src/components/ContactUs/index.tsx
@@ -1,37 +1,28 @@
-import React from 'react';
-import { FormControl, InputLabel, Input,Button } from "@material-ui/core";
+import { FormControl, TextField } from "@material-ui/core";
 import styles from './styles.module.scss';
 import globalStyles from '../styles/styles.module.scss';
 
 export const ContactUs = () => {
 
     return (
-        <section >
-            <div className={styles.contactUsBackground}>
-                <h1 className={styles.sectionMainTitle}>Contact Us</h1>
-                <div className={styles.contactUsForm}>
-                    <form>
-                        <FormControl margin="dense" fullWidth>
-                        <InputLabel htmlFor="name">Name</InputLabel>
-                        <Input id="name" type="text" />
-                        </FormControl>
+            <section className={styles.section}> 
+                <div className={styles.content}>
+                <h2 className={globalStyles.sectionMainTitleDark}>Contact Us</h2>
+                <form className={styles.form} autoComplete="off">
+                    <FormControl margin="dense" fullWidth>
+                    <TextField required id="outlined-basic" label="Name" variant="outlined"/>
+                    </FormControl>
 
-                        <FormControl margin="dense" fullWidth>
-                        <InputLabel htmlFor="email">Email</InputLabel>
-                        <Input id="email" type="email" />
-                        </FormControl>
+                    <FormControl  margin="dense" fullWidth>
+                    <TextField required id="outlined-basic" label="Email" variant="outlined"/>
+                    </FormControl>
 
-                        <FormControl margin="dense" fullWidth>
-                        <InputLabel htmlFor="message">Message</InputLabel>
-                        <Input id="message" multiline rows={6} />
-                        </FormControl>
-
-                        <Button variant="contained" className={globalStyles.button}>Send</Button>
-                    </form>
+                    <FormControl margin="dense" fullWidth>
+                    <TextField required id="outlined-helperText" label="Message" variant="outlined" helperText="* Required" multiline rows={6} />
+                    </FormControl>
+                </form>
+                <button className={globalStyles.button}>Send</button>
                 </div>
-            </div>
-        </section>
-     
+            </section>
     );
-
 }
